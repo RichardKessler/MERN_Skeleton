64a13117ddc0a79d1de488bd5ced28a952dc4db0
JidMenosta <jidmenosta@gmail.com>
2021-06-12 20:43:35 -0400

    added express.js and server.js files with starting code to listen for a server and accept requests.

---
+++ server/express.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+
+
+
+
+const app = express()
+
+// parse body and attache the to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// secure app by setting various HTTP headers
+app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+
+
+export default app
