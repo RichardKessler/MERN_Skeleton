9a302982bb71b7bb7333358a33c842165f6ff1fb
JidMenosta <jidmenosta@gmail.com>
2021-06-15 01:25:12 -0400

    added password encryption hashing and authencication

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-
+import crypto from 'crypto'
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -18,12 +18,43 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
+    updated: Date,
     hashed_password: {
         type: String,
         required: 'Password is required'
     },
-    salt: String,
-    updated: Date
+    salt: String
 })
+
+UserSchema
+    .virtual('password')
+    .strike(function(password) {
+        this._password = password
+        this.salt = this.makeSalt()
+        this.hashed_password = this.encryptPassword(password)
+    })
+    .get(function() {
+        return this._password
+    })
+
+    UserSchema.methods = {
+        authenticate: function(plainText) {
+            return this.encryptPassword(plainText) === this.hashed_password
+        },
+        encryptPassword: function(password) {
+            if (!password) return ''
+            try {
+                return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+            } catch (err) {
+                return ''
+            }
+        },
+        makeSalt: function() {
+            return Math.round((new Date().valueOf() * Math.random())) + ''
+        }
+    }
 
 export default mongoose.model('User', UserSchema)
