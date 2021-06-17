9136220afa80bbd62f4f7b4aa38a341553d96802
JidMenosta <jidmenosta@gmail.com>
2021-06-16 22:01:25 -0400

    kljasd

--- server/express.js
+++ server/express.js
@@ -6,16 +6,33 @@ import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
 import Template from './../template'
+import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
 
+// comment out before production
+import devBundle from './devBundle'
 
+const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 
+// comment out before production
+devBundle.compile(app)
+
+// parse body params and attache them to req.body
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
+// secure apps by setting variou HTTP headers
 app.use(helmet())
+// anable CORS - Cross Origin Resouce Sharing
 app.use(cors())
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+// mount routes
+app.use('/', userRoutes)
+app.use('/', authRoutes)
 
 
 app.get('/', (req, ers) => {
