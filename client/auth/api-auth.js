58c8a8a9a7f6b9c7b998874c15edb9a98e72a2dd
JidMenosta <jidmenosta@gmail.com>
2021-06-18 16:31:10 -0400

    added auth files and signin code for users

---
+++ client/auth/api-auth.js
@@ -0,0 +1,20 @@
+const signin = async (user) => {
+    try {
+        let response = await fetch('/spi/signin/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify(user)
+        })
+        return await response.json()
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+const signout = async (user) => {
+    
+}
