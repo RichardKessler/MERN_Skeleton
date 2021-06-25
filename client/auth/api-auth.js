05e5c284db083e378fd1c97b5382463a15deac74
JidMenosta <jidmenosta@gmail.com>
2021-06-24 21:27:32 -0400

    added api authentication routes

--- client/auth/api-auth.js
+++ client/auth/api-auth.js
@@ -1,6 +1,6 @@
 const signin = async (user) => {
     try {
-        let response = await fetch('/spi/signin/', {
+        let response = await fetch('/api/signin/', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -16,5 +16,15 @@ const signin = async (user) => {
 }
 
 const signout = async (user) => {
-    
+    try {
+        let response = await fetch('/auth/signout/', { method: 'GET' })
+        return await response.json()
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export {
+    signin,
+    signout
 }
