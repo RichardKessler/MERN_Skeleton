17902bde2011e2a0de73c69ebe03adaee7e11565
JidMenosta <jidmenosta@gmail.com>
2021-06-26 23:12:25 -0400

    added the signup component and route path

---
+++ client/user/Signup.js
@@ -0,0 +1,75 @@
+import { CardContent, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, Typography } from "@material-ui/core"
+import { CallMissedSharp } from "@material-ui/icons"
+import { create } from "./api-user.js"
+
+
+export default function Signup() {
+
+    const [values, setValues] = useState({
+        name: '',
+        password: '',
+        email: '',
+        open: false,
+        error: ''
+    })
+
+    const handleChange = name => event => {
+        setValues({ ...values, [name]: event.target.value })
+    }
+
+    const clickSubmit = () => {
+        const user = {
+            name: values.name || undefined,
+            email: values.email || undefined,
+            password: values.password || undefined
+        }
+        create(user).then((data) => {
+            if (data.error) {
+                setValues({ ...values, error: data.error})
+            } else {
+                setValues({ ...values, error: '', open: true})
+            }
+        })
+    }
+
+    return (
+        <div>
+            <Card className={CallMissedSharp.card}>
+                <CardContent>
+                    <Typography varient="h6" className={classes.title}>
+                        Sign Up
+                    </Typography>
+                    <TextField id="name" label="Name" className={classes.textField} value={values.name} onchange={handleChange('name')} margin="normal"/>
+                    <br/>
+                    <TextField id="email" type="email" label="Email" className={classes.textField} value={values.email} onchange={handleChange('email')} margin="normal"/>
+                    <br/>
+                    <TextField id="password" type="password" label="Password" className={classes.textField} value={values.password} onChange={handleChange('password')} margin="normal"/>
+                    <br/> {
+                        values.error && (<Typography component="p" color="error">
+                            <Icon color="error" className={classes.error}>error</Icon>
+                            {values.error}
+                        </Typography>)
+                    }
+                </CardContent>
+                <CardActions>
+                    <Button color="primary" varient="contained" onClick={clickSubmit} className={classes.submit}>Submit</Button>
+                </CardActions>
+            </Card>
+            <Dialog open={values.open} disableBackdropClick={true}>
+                <DialogTitle>New Account</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        New account successfully created.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Link to="/signin">
+                        <Button color="primary" autoFocus="autoFocus" varient="contained">
+                            Sign In
+                        </Button>
+                    </Link>
+                </DialogActions>
+            </Dialog>
+        </div>
+    )
+}
