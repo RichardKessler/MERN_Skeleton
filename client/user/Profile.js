fa8a949dc6f31a9d2a0312b6d845157c16ac2317
JidMenosta <jidmenosta@gmail.com>
2021-06-28 00:30:29 -0400

    adding moer profile code so the user will see certain elements

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -28,5 +28,29 @@ export default function Profile({ match }) {
 
     }, [match.params.userId])
 
-    
+    if (redirectToSignin) {
+        return <Redirect to='/signin/'/>
+    }
+    return (
+        <Paper className={classes.root} elevation={4}>
+            <Typography varian="h6" className={classes.title}>
+                Profile
+            </Typography>
+            <List dense>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar>
+                            <Person/>
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={user.name} secondary={user.email}/>
+                </ListItem>
+                <Divider/>
+                <ListItem>
+                    <ListItemText primary={"Joined: " + (
+                        new Date(user.created)).toDateString()}/>
+                </ListItem>
+            </List>
+        </Paper>
+    )
 }
