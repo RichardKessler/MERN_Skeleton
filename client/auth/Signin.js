f90c59d7b94571b82b27a2ec1c02d220bfa61514
JidMenosta <jidmenosta@gmail.com>
2021-06-27 21:34:28 -0400

    added signin function for users

---
+++ client/auth/Signin.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { signin } from './api-auth'
+import auth from './auth-helper'
+
+
+export default function Signin(proprs) {
+    const classes = useStyles()
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        error: '',
+        redirectToReferrer: false
+    })
+
+    const clickSubmit = () => {
+        const user = {
+            email: values.email || undefined,
+            password: values.password || undefined
+        }
+
+        signin(user).then((data) => {
+            if (data.error) {
+                setValues({ ...values, error: data.error})
+            } else {
+                auth.authenticate(data, () => {
+                    setValues({ ...values, error: '',redirectToReferrer: true})
+                })
+            }
+        })
+    }
+
+    const {from} = props.location.state || {
+        from: {
+            pathname: '/'
+        }
+    }
+    const {redirectToReferrer} = values
+    if (redirectToReferrer) {
+        return (<Redirect to={from}/>)
+    }
+
+    
+}
